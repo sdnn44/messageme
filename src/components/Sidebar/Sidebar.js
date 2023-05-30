a26352e7beb660e3fc4d569c441c825bdcf34ca5
sdnn44 <slidennnq@gmail.com>
2023-05-30 23:01:57 +0200

    added header for sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,25 +1,35 @@
-import { Box } from '@mui/material';
-import React from 'react'
-import styled from 'styled-components';
-import { SidebarBody } from './SidebarBody';
-import { SidebarHeader } from './SidebarHeader';
+import { Box } from "@mui/material";
+import React from "react";
+import styled from "styled-components";
+import { SidebarBody } from "./SidebarBody";
+import { SidebarHeader } from "./SidebarHeader";
+import { Divider } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const Wrapper = styled.div`
-  display: inline-block;
-  vertical-align: text-top;
-  margin: 0 auto;
-  background-color: black;
+  background-color: rgb(26, 26, 33);
+  width: 30vh;
   color: white;
 `;
 
 const Sidebar = () => {
+  const theme = useTheme();
+  const style = {
+    mt: 2,
+    width: "90%",
+    maxWidth: 360,
+    bgcolor: theme.palette.primary.main,
+  };
+
   return (
     <>
-    <Wrapper>Sidebar</Wrapper>
-    {/* <SidebarBody />
-    <SidebarHeader /> */}
+      <Wrapper>
+        <SidebarHeader />
+        <SidebarBody />
+        <Divider sx={style} variant="middle" />
+      </Wrapper>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/components/Sidebar/SidebarBody.js
+++ src/components/Sidebar/SidebarBody.js
@@ -1,7 +1,49 @@
 import React from 'react'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+display: flex;
+align-items: center;
+height: 1rem;
+padding: 1rem;
+`;
+
+const Search = styled.div`
+display: flex;
+align-items: center;
+width: 100%;
+background-color: rgb(31,38,49);
+border-radius: 50px; 
+
+input {
+  border: none;
+  outline: none;
+  background-color: rgb(31,38,49);
+  color: #fff;
+}
+
+svg {
+  padding .5rem;
+  color: rgb(1, 86, 189);
+}
+`;
 
 export const SidebarBody = () => {
   return (
-    <div>SidebarBody</div>
+    /*Szukaj divider przypięty all chats */
+    <Wrapper>
+      <Search>
+      {/* <Input icon={<SearchOutlinedIcon />}>
+          
+        </Input> */}
+        <SearchOutlinedIcon />
+        <input
+            type='text'
+            placeholder='Wyszukaj czat...'
+          />
+      </Search>
+      {/* jakis tekst */}
+    </Wrapper>
   )
 }
